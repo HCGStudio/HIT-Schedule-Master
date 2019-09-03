903eb7a12242ea2c2cb81d28012fe62b79728764
mahoshojoHCG <yzy688@gmail.com>
2019-09-03 21:51:18 +0800

    Fix Some Bugs

--- HITScheduleMaster/App.xaml.cs
+++ HITScheduleMaster/App.xaml.cs
@@ -21,7 +21,7 @@ namespace HCGStudio.HITScheduleMaster
             http.DefaultRequestHeaders.UserAgent.ParseAdd(
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15");
             var info = JsonConvert.DeserializeObject<dynamic>(await (await http.GetAsync(
-                    "https://api.github.com/repos/HCG-Studio/HIT-Schedule-Master/releases/latest")).Content
+                    "https://api.github.com/repos/HCGStudio/HIT-Schedule-Master/releases/latest")).Content
                 .ReadAsStringAsync());
             if (string.Compare(currentVersion, info.tag_name as string, StringComparison.Ordinal) >= 0) return;
             if (MessageBox.Show("有新版本发布，是否更新？", "新版本！", MessageBoxButton.YesNo, MessageBoxImage.Asterisk) ==
