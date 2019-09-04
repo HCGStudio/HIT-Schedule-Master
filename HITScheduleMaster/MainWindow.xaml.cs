132b4ddc478716c6a3bf2a84b5245f2aa7146939
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2019-09-04 18:36:32 +0800

    完善编辑课表功能。

--- HITScheduleMaster/MainWindow.xaml.cs
+++ HITScheduleMaster/MainWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Text;
 using System.Collections.ObjectModel;
 using System.IO;
 using Newtonsoft.Json;
+using System.Windows;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
 
 namespace HCGStudio.HITScheduleMaster
 {
@@ -14,14 +17,38 @@ namespace HCGStudio.HITScheduleMaster
         {
             InitializeComponent();
             Model = new ScheduleModel();
-            this.DataContext = Model;
+            DataContext = Model;
             
 
         }
 
-        private void Login_Button_OnClick(object sender, System.Windows.RoutedEventArgs e)
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+            => Environment.Exit(0);
+
+        private void RideOn_OnClick(object sender, RoutedEventArgs e)//QuickExport
+            => Model.Export();
+
+        private void RemoveCourses_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (CourseView.SelectedItems.Count!=0)
+            {
+                foreach (var item in CourseView.SelectedItems)
+                {
+                    Model.Courses.Remove((Course)item);
+                }
+            }
+        }
+
+        private void ImportCourses_OnClick(object sender, RoutedEventArgs e)
         {
 
         }
+        private void ExportCourses_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void NotifyTime_PreviewTextInput(object sender, TextCompositionEventArgs e)
+            => e.Handled = (new Regex("[^0-9]+")).IsMatch(e.Text);
+        //Ensure the input is a positive integer
     }
 }
--- HITScheduleMaster/ScheduleModel.cs
+++ HITScheduleMaster/ScheduleModel.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using Ical.Net;
+using Ical.Net.Serialization;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace HCGStudio.HITScheduleMaster
 {
@@ -9,6 +16,43 @@ namespace HCGStudio.HITScheduleMaster
     {
         public ObservableCollection<Course> Courses { get; set; }
 
+        //把 ObservableCollection<Course> 转成 Calendar
+        public Calendar ToCalendar()
+        {
+            throw new NotImplementedException();
+        }
 
+
+        public override string ToString()
+            => new CalendarSerializer().SerializeToString(ToCalendar());
+        
+        public void  Export()
+        {
+            var dialog = new SaveFileDialog
+            {
+                Filter = "iCalendar 文件 (*.ics)|*.ics",
+                Title = "保存文件",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                FileName = $"课表.ics"
+            };
+
+            dialog.ShowDialog();
+
+            try
+            {
+                File.WriteAllText(dialog.FileName, ToString(), new UTF8Encoding(false));
+            }
+            catch
+            {
+                MessageBox.Show("写入出错！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (MessageBox.Show("成功，您是否要了解如何将iCalendar导入到您的日历中？", "导出成功", MessageBoxButton.YesNo,
+                    MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Process.Start("explorer", "https://github.com/HCGStudio/HIT-Schedule-Master/wiki");
+            }
+
+        }
     }
 }
