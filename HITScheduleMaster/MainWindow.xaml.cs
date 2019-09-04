8824d348f36f38408bc0ce6757131871a3ac5cd7
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2019-09-04 18:47:45 +0800

    完成了大部分编辑课表功能

--- HITScheduleMaster/MainWindow.xaml.cs
+++ HITScheduleMaster/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using Newtonsoft.Json;
 using System.Windows;
 using System.Text.RegularExpressions;
 using System.Windows.Input;
+using System.Diagnostics;
+using Microsoft.Win32;
 
 namespace HCGStudio.HITScheduleMaster
 {
@@ -45,7 +47,40 @@ namespace HCGStudio.HITScheduleMaster
         }
         private void ExportCourses_OnClick(object sender, RoutedEventArgs e)
         {
+            
+            if (CourseView.SelectedItems.Count != 0)
+            {
+                
+                
+                var dialog = new SaveFileDialog
+                {
+                    Filter = "JSON 文件 (*.json)|*.json",
+                    Title = "保存文件",
+                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    FileName = $"课表.ics"
+                };
 
+                dialog.ShowDialog();
+
+                try
+                {
+                    File.WriteAllText(dialog.FileName, 
+                        JsonConvert.SerializeObject(CourseView.SelectedItems), 
+                        new UTF8Encoding(false));
+                }
+                catch
+                {
+                    MessageBox.Show("写入出错！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                if (MessageBox.Show("成功，您是否要了解如何将分享或重利用导出的课程？", "导出成功", MessageBoxButton.YesNo,
+                        MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    Process.Start("explorer", "https://github.com/HCGStudio/HIT-Schedule-Master/wiki");
+                }
+
+
+            }
         }
         private void NotifyTime_PreviewTextInput(object sender, TextCompositionEventArgs e)
             => e.Handled = (new Regex("[^0-9]+")).IsMatch(e.Text);
