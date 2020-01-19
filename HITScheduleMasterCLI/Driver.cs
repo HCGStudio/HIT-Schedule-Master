4f6652bc11bc9f7a15912fc6a57c86339cf839f4
宿梓航 <Ferdinandsukhoi@outlook.com>
2020-01-19 15:57:06 +0800

    完成ScheduleTranslator

---
+++ HITScheduleMasterCLI/Driver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MobileSuit;
+using ScheduleTranslator;
+using MobileSuit.IO;
+
+namespace HITScheduleMasterCLI
+{
+    public class Driver : IIoInteractive
+    {
+        public void A()
+        {
+            for (; ; )
+                ShowScheduleEntry(new ScheduleEntry(DayOfWeek.Monday, CourseTime.C12, Io?.ReadLine()));
+        }
+        private void ShowScheduleEntry(ScheduleEntry entry)
+        {
+            var outList = new List<(string, ConsoleColor?)>
+            {
+                (entry.CourseName.PadRight(24,' '), null),
+                (entry.DayOfWeekName, null),
+                ("\t", null),
+                (entry.CourseTimeName, null),
+                ("\t", null),
+                (entry.StartTime.ToString(), null),
+                ("\t", null),
+                (entry.Teacher, null),
+                ("\t", null),
+                (entry.Location==""?"\t":entry.Location, null),
+                ("\t", null),
+                (entry.IsLongCourse ? "2" : "3", null),
+                ("\t", null)
+            };
+            for (var i = entry.Week >> 1; i != 0; i >>= 1)
+            {
+
+                if ((i & 1) == 1)
+                {
+                    outList.Add((" 1  ", ConsoleColor.Green));
+                }
+                else
+                {
+                    outList.Add((" 0  ", ConsoleColor.Red));
+                }
+            }
+
+            Io?.WriteLine(outList);
+        }
+        private IoInterface Io { get; set; }
+
+        public void SetIo(IoInterface io)
+        {
+            Io = io;
+        }
+    }
+}
---
+++ HITScheduleMasterCLI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using ScheduleTranslator;
+using MobileSuit;
+
+namespace HITScheduleMasterCLI
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            (new MobileSuitHost(typeof(Driver))).Run();
+            var regex = new Regex(@"\d+");
+            while (true)
+            {
+                var col = regex.Matches(Console.ReadLine()??"");
+                foreach (var match in col)
+                {
+                    Console.WriteLine(match);
+                }
+                
+            }
+            
+        }
+        
+
+    }
+}
